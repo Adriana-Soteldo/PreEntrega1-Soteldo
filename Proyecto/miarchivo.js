79696ad16d0ae7b0b8bee33aa233a982cd310b56
Adriana Soteldo <asoteldo@futbolsites.net>
2023-04-10 22:43:13 -0300

    2daEntrega+ASoteldo

--- Proyecto/miarchivo.js
+++ Proyecto/miarchivo.js
@@ -1,7 +1,9 @@
-alert("A un paso de tu Primera Cuenta de Ahorros")
-const nombre = prompt("Ingrese su nombre")
-const apellido = prompt("Ingrese su apellido")
-let edad = prompt("Ingrese su edad")
+alert("Y si empezamos a Ahorrar?")
+const nombre = prompt("Nombrecito")
+const apellido = prompt("Y te apellidas")
+let edad = prompt("No tengas pena y dime tu edad")
+
+const cliente = {nombre, apellido , edad, id:"", ciudad:"", telefono:""} 
 
 if (edad >= 18){
     esMayorDeEdad()
@@ -12,7 +14,7 @@ if (edad >= 18){
 }
 
 function crearCuenta(){
-    let id = prompt("Indicame tu ID") 
+    let id = prompt("Indicame tu DNI") 
     let idValido = false
     
     for (let i = 1; i<= id.length ; i++){
@@ -22,6 +24,13 @@ function crearCuenta(){
     }
     if (idValido) {
     console.log ("Gracias estas mas cerca") 
+    alert("Continuemos con el proceso")
+        let ciudad = prompt("Dire")
+        let telefono = prompt("y telefono")
+        cliente.ciudad = ciudad
+        cliente.telefono = telefono
+        cliente.id = id
+
     } else {
         console.log("ID invalido intentalo de nuevo")
     }
@@ -34,5 +43,34 @@ function esMayorDeEdad(){
 
 function esMenorDeEdad(){
     console.log("Lo siento " + nombre + " " + apellido + " no cumples con la condicion")
-    
+
 }
+
+console.log ("Datos de Cliente: ",cliente)
+console.log (cliente.apellido)
+console.log (cliente.edad)
+console.log (cliente.id)
+console.log (cliente.ciudad)
+console.log (cliente.telefono)
+
+
+alert("Que tipo de cuenta deseas?")
+const arrayCuenta = ["ahorros", "corriente", "dolares"]
+let tipoDeCuenta =prompt("Seleccione el tipo de cuenta: \n" + arrayCuenta.map(function(tipo,i){  
+    return (i+1) + " - " + tipo
+
+}).join("\n"))
+
+alert("Gracias por seleccionar la cuenta de " + arrayCuenta [tipoDeCuenta-1] )
+
+datosBancarios = {sucursal:"018" , producto:tipoDeCuenta, cuenta:random()}
+console.log ("Haz completado la fase Inicial, tu cuenta es: " + datosBancarios.sucursal + "-" + datosBancarios.producto + "-" + datosBancarios.cuenta)
+
+function random() {
+    let cuenta = ""
+    for (i=0 ; i<5 ; i++) {
+    cuenta+= Math.abs(Math.floor((Math.random() * (0 - 9 + 1)) + 0));    
+    }
+   return cuenta
+}
+
